ff289bcb83e396f57eac3f64ba683b82fb1ae6cb
QOAL <qoal@qplanner.co.uk>
2021-08-09 14:24:16 +0100

    1.11.1 Add a missing line
    Whoops!

--- localisetimes.js
+++ localisetimes.js
@@ -1,4 +1,6 @@
 "use strict";
+
+let dateTimeFormats = Array(2)
 //PHP spat these timezone abbreviations out, and their offsets - There could be some missing
 const tzaolObj = {"GMT":0,"EAT":180,"CET":60,"WAT":60,"CAT":120,"EET":120,"WEST":60,"WET":0,"CEST":120,"SAST":120,"HDT":-540,"HST":-600,"AKDT":-480,"AKST":-540,"AST":-240,"EST":-300,"CDT":-300,"CST":-360,"MDT":-360,"MST":-420,"PDT":-420,"PST":-480,"EDT":-240,"ADT":-180,"NST":-210,"NDT":-150,"NZST":720,"NZDT":780,"EEST":180,"HKT":480,"WIB":420,"WIT":540,"IDT":180,"IST":330,"PKT":300,"WITA":480,"KST":540,"JST":540,"ACST":570,"ACDT":630,"AEST":600,"AEDT":660,"AWST":480,"BST":60,"MSK":180,"SST":-660,"UTC":0,"PT":0,"ET":0,"MT":0,"CT":0};
 const tzaolStr = Object.keys(tzaolObj).join("|");
